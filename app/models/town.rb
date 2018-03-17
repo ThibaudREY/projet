3569e6912e5806181fd6cf70f6e23591aed6dcdb
Thibaud REY <contact@thibaudrey.pro>
2018-03-17 09:55:49 +0100

    City geolocation refactoring

--- app/models/town.rb
+++ app/models/town.rb
@@ -4,12 +4,12 @@ class Town < ActiveRecord::Base
 
   private
   def geocode
-    places = Nominatim.search.city(self.city).limit(1).address_details(true)
-    for place in places
-      self.latitude = place.latitude
-      self.longitude = place.longitude
-      self.postcode = place.address.postcode
-    end
+    place = Nominatim.search.city(self.city).limit(1).address_details(true).first
+
+    self.latitude = place.latitude
+    self.longitude = place.longitude
+    self.postcode = place.address.postcode
+
   end
 
 end
